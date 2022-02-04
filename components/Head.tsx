232d68dcf360ac182ee645312daf5d5490a490a2
변찬우 <chan00106@naver.com>
2022-02-04 09:09:57 +0000

    Add twittercard

---
+++ components/Head.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+
+function H() {
+  return (
+    <Head>
+      <title>Dashborad</title>
+      <meta name="description" content="아무생각 없이 만든 사이트" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Dashborad" />
+      <meta property="og:url" content="https://araboza.kro.kr" />
+      <meta
+        property="og:image"
+        content="https://i.pinimg.com/736x/41/fc/67/41fc6786880e28322afb4c4ef42c679c--fanart-legends.jpg"
+      />
+      <meta property="og:description" content="아무생각 없이 만든 사이트" />
+      <meta name="twitter:card" content="summary" />
+      <meta property="twitter:title" content="Dashborad" />
+      <meta property="twitter:url" content="https://araboza.kro.kr" />
+      <meta
+        property="twitter:image"
+        content="https://i.pinimg.com/736x/41/fc/67/41fc6786880e28322afb4c4ef42c679c--fanart-legends.jpg"
+      />
+      <meta
+        property="twitter:description"
+        content="아무생각 없이 만든 사이트"
+      />
+    </Head>
+  );
+}
+
+export default H;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import H from "../components/Head";
 import Header from "./Header";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
+      <H />
       <Header />
       <main>{children}</main>
     </>
